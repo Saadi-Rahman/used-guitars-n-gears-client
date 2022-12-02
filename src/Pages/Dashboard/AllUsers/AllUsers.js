180497e980a6b72932490cdeb0c7384ee7be7633
Saadi-Rahman <sadi6280@gmail.com>
2022-12-02 13:02:39 +0600

    added PUT method to handle admin role

--- src/Pages/Dashboard/AllUsers/AllUsers.js
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -1,15 +1,32 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllUsers = () => {
-    const {data: users = []} = useQuery({
+    const {data: users = [], refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async() =>{
             const res = await fetch('http://localhost:5000/users');
             const data = await res.json();
             return data;
         }
-    })
+    });
+
+    const handleMakeAdmin = id => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount > 0){
+                toast.success('Make Admin Successful!')
+                refetch();
+            }
+        })
+    }
 
     return (
         <div>
@@ -36,7 +53,11 @@ const AllUsers = () => {
                                     <th className="bg-transparent">{i+1}</th>
                                     <td className="bg-transparent">{user.name}</td>
                                     <td className="bg-transparent">{user.email}</td>
-                                    <td className="bg-transparent"><button className='btn btn-xs btn-outline btn-primary'>Make Admin</button></td>
+                                    <td className="bg-transparent">
+                                        { user?.role !== 'admin' && 
+                                            <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-outline btn-primary'>Make Admin</button> 
+                                        }
+                                    </td>
                                     <td className="bg-transparent">
                                         <button className="btn btn-xs btn-square btn-outline btn-secondary">
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
